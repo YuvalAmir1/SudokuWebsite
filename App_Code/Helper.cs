2b3c01a8f206959177b018a3ac3df8a72b9ca464
YuvalAmir1 <yuvalamir1111@gmail.com>
2022-04-13 20:17:21 +0300

    0.5

---
+++ App_Code/Helper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SudokuWebsite.App_Code
+{
+    public static class Helper
+    {
+        public const string databaseName = "Database.mdf";
+        public const string tableName = "Users";
+        public const string conString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\" + databaseName + ";Integrated Security=True";
+
+        public static DataSet RetriveTable(string SQLStr)
+        {
+            SqlConnection con = new SqlConnection(conString);
+            SqlCommand cmd = new SqlCommand(SQLStr, con);
+            SqlDataAdapter ad = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+
+            ad.Fill(ds, tableName);
+            return ds;
+        }
+
+
+        // Continue Here:
+        public static string BuildUserTableHTML(DataTable dt)
+        {
+            string HTMLStr = "<table class='usersTable' align='center'>";
+            foreach (DataColumn column in dt.Columns)
+                HTMLStr += "<th>" + column.ColumnName + "</th>";
+
+                HTMLStr += "</table>";
+            return HTMLStr;
+        }
+    }
+}
